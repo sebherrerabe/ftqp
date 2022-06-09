68d39a39c783e30e8892fa6a9968d77763a8dadb
sebherrerabe <sebastianherrerac1997@gmail.com>
2022-06-09 17:20:24 +0200

    titles done

--- src/Sections/Components/Titles/Titles.jsx
+++ src/Sections/Components/Titles/Titles.jsx
@@ -1,12 +1,14 @@
 
 import './Titles.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const TitleLeft = ({ icon, title }) => {
-    return (<div className="title-left">
-        <h2>{icon}{title}</h2>
+    return (<div className="title title-left">
+        <h2><FontAwesomeIcon icon={icon} />{title}</h2>
     </div>);
 }
-
-export const TitleRight = () => {
-    return (<></>);
+export const TitleRight = ({ icon, title }) => {
+    return (<div className="title title-right">
+        <h2><FontAwesomeIcon icon={icon} />{title}</h2>
+    </div>);
 }
--- src/Sections/Team/Team.jsx
+++ src/Sections/Team/Team.jsx
@@ -3,13 +3,12 @@
 import { TitleLeft } from '../Components/Titles/Titles';
 
 import './Team.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faPeopleGroup } from '@fortawesome/free-solid-svg-icons';
 
 const Team = () => {
     return (<div className="main-container" id='notre-equipe'>
         <div className="team-container">
-            <TitleLeft title="Notre equipe" />
+            <TitleLeft title="Notre equipe" icon={faPeopleGroup} />
         </div>
     </div>);
 }
