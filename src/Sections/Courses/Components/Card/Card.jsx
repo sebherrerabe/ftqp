4a0d6d64c60ffca4bdc02a7ce385f9877b5a7dfb
OmerAslani <omersatelit21@gmail.com>
2022-06-13 17:30:00 +0200

    hi

--- src/Sections/Courses/Components/Card/Card.jsx
+++ src/Sections/Courses/Components/Card/Card.jsx
@@ -9,6 +9,33 @@ const Card = ({ children, img, direction, title, desc , list }) => {
 
     const [cardClass] = useState(direction === 'left' ? 'left' : 'right');
 
+
+
+    const text = [
+        {
+            'id':1,
+            'text':"Inscription toute l’année",
+        }
+        {
+            'id':1,
+            'text':"Formations qualifiante /+ou - 9 mois / Lundi - Jeudi",
+        }
+        {
+            'id':1,
+            'text':"Stage en entreprise / 4 semaines / Lundi - Vendred",
+        }
+        {
+            'id':1,
+            'text':"Objectif: mise à l’emploi",
+        }
+        {
+            'id':1,
+            'text':"Contrat BF: 2€/heure, etc.",
+        }
+    ]
+
+
+
     // const  myloop = () =>{
 
     //     const mylists = [
