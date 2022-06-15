ff5c1cc00f153d06555e158bd7180693f0490120
sebherrerabe <sebastianherrerac1997@gmail.com>
2022-06-15 11:43:21 +0200

    video's name correctly changed, html width deleted

--- src/Sections/Header/Header.jsx
+++ src/Sections/Header/Header.jsx
@@ -3,6 +3,7 @@
 // COMPONENTS IMPORTS
 import NavBar from './Components/Navbar/Navbar';
 import Hero from './Components/Hero/Hero';
+import bgvideo from './assets/bgvideo.mp4';
 
 
 // STYLING IMPORTS
@@ -10,8 +11,8 @@ import './Header.css'
 
 const Header = () => {
     return (<header className="main-container" id='accueil' >
-        <video width="400" className="bg-video" autoPlay muted loop>
-            <source src="SansTitre.mp4" type="video/mp4"></source>
+        <video className="bg-video" autoPlay loop muted>
+            <source src={bgvideo} type="video/mp4"></source>
         </video>
         <NavBar />
         <Hero />
