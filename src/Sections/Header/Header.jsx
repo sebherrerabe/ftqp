4415754829ea0c004b2a88a799eb3ada1ddea518
sebherrerabe <sebastianherrerac1997@gmail.com>
2022-06-16 11:30:20 +0200

    hero finished

--- src/Sections/Header/Header.jsx
+++ src/Sections/Header/Header.jsx
@@ -12,9 +12,10 @@ import './Header.css'
 const Header = () => {
 
 
-    
 
-    return (<header  className="section main-container" id='accueil' >
+
+    return (<header className="section main-container" id='accueil' >
+        <div className="gradient"></div>
         <video className="bg-video" autoPlay loop muted>
             <source src={bgvideo} type="video/mp4"></source>
         </video>
