2b4b44071764384410151518812da95a28f29f6b
sebherrerabe <sebastianherrerac1997@gmail.com>
2022-06-17 12:16:35 +0200

    all god

--- src/Sections/Courses/Courses.jsx
+++ src/Sections/Courses/Courses.jsx
@@ -1,117 +1,84 @@
 import Title from "../Components/Title/Title";
-import Card from "./Components/Card/Card";
 
 
 import "./Courses.css";
 import { faPersonDigging } from "@fortawesome/free-solid-svg-icons";
 import plafonnage from './assets/plafonnage.jpg';
+import bg from './assets/bg.jpg';
+
+const Row = ({ left, right }) => {
+    return <div className="courses-row mg-top-40">
+        <div className="row-left">
+            {left}
+        </div>
+        <div className="row-right">
+            {right}
+        </div>
+    </div>
+}
+
+const InfoCard = ({ children, direction }) => {
+    return <div className={`info-card ${direction === 'left' ? "mg-left-20" : "mg-right-20"}`}>
+        {children}
+    </div>
+}
+
+const CourseCard = ({ title, img, direction }) => {
+    return <div className={`course-card ${direction === 'left' ? "mg-left-20" : "mg-right-20"}`}>
+        <div className="course-card-top">
+            <h3 className={direction === 'left' ? "h3-left" : "h3-right"}>{title}</h3>
+        </div>
+        <div className="course-card-bottom">
+            <img src={img} alt="" />
+        </div>
+    </div>
+}
+
+const Desc = () => {
+    return <Row left={
+        <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga quas impedit quos tempora nulla, veniam ab repellendus a! Illo
+            inventore aperiam assumenda beatae fugiat tenetur quisquam nemo nihil sunt minus.</p>
+    } right={
+        <InfoCard direction="left"></InfoCard>
+    } />
+}
+
+const Plafonnage = () => {
+    return <Row left={
+        <CourseCard title="Plafonnage" img={plafonnage} direction="right" />
+    } right={
+        < InfoCard direction="left">
+
+        </InfoCard >
+    } />
+}
+
+const Maconerie = () => {
+    return <Row left={
+        < InfoCard >
+
+        </InfoCard >
+    } right={
+        <CourseCard title="Maconerie" img={plafonnage} direction="left" />
+    } />
+
+}
 
 const Courses = () => {
-    const text = [
-        {
-            id: 1,
-            text: "Inscription toute l année",
-        },
-        {
-            id: 2,
-            text: "Formations qualifiante /+ou - 9 mois / Lundi - Jeudi",
-        },
-        {
-            id: 3,
-            text: "Stage en entreprise / 4 semaines / Lundi - Vendred",
-        },
-        {
-            id: 4,
-            text: "Objectif: mise à l emploi",
-        },
-        {
-            id: 5,
-            text: "Contrat BF: 2€/heure, etc.",
-        }
-    ]
+
+
     return (
         <section className="section main-container" id="nos-formations">
-            <div className="courses-container">
-                <Title title="Nos Formations" icon={faPersonDigging} direction="right"  />
-                <div className="section-desc mg-top-40">
-                    <p className="desc mg-top-30">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia
-                        fuga accusamus sed saepe. Sint tempora incidunt, ducimus inventore
-                        officiis vitae temporibus. Libero distinctio unde facere aperiam? Sunt
-                        nihil deleniti vel.
-                    </p>
-                    <h3 className="mg-top-40">Nos conditions :</h3>
-                    <ul className="mg-top-30">
-                        <li> Avoir plus de 18 ans. </li>
-                        <li> Etre détenteur d'un CEB ou d'un CESI ou le niveau équivalent. </li>
-                        <li> Etre inscrit comme demandeur d'emploi et titulaire du document A15. </li>
-                    </ul>
-                </div>
-                <Card direction='left' img={plafonnage} title="Plafonnage" desc=" Avec des techniques 
-                manuelles et mécaniques,le plafonneur sera capable de réaliser, réparer et décorer et maîtrisera les techniques de mortier, cimentage et d'enduit."
-
-                list={ <ul>
-                    {text.map(el => <li key={el.id}>{el.text}</li>)}
-                </ul>} >
-
-                </Card>
-                <Card direction='right' img={plafonnage} title="Maçonnerie" desc="  Outre la maçonnerie, le stagiaire pourra réaliser des travaux di'solation
-                        et d'étanchéité, des coffrages et ferraillages, du bétonnage,
-                        jointoiement et égouttage."
-                    list={<ul>
-
-                        {text.map(el => <li key={el.id}>{el.text}</li>)}
-
-                    </ul>} >
-                </Card>
-            </div>
-
-
-            {/* 
-            
-            <div className="pic-container">
-                <div className="pic"></div>
-                <div className="cards">
-                    <div className="card-content">
-                        <h3 className="margin-bottom">Plafonnage</h3>
-                        <p>
-
-                            Avec des techniques manuelles et mécaniques,le plafonneur sera
-                            capable de réaliser, réparer et décorer et maîtrisera les
-                            techniques de mortier, cimentage et d'enduit.
-                        </p>
-                        <ul>
-                            <li>Inscription toute l’année</li>
-                            <li>Inscription toute l’année</li>
-                            <li>Inscription toute l’année</li>
-                            <li>Inscription toute l’année</li>
-                            <li>Inscription toute l’année</li>
-                        </ul>
+            <div className="bg-photo" style={{ backgroundImage: `url(${bg})` }}>
+                <div className="courses-container">
+                    <Title title="Nos Formations" icon={faPersonDigging} direction="right" />
+                    <div className="section-desc mg-top-40">
+                        <Desc />
+                        <Plafonnage />
+                        <Maconerie />
                     </div>
                 </div>
             </div>
-
-            <div className="pic-container1">
-                <div className="cards1">
-                    <div className="card-content">
-                        <h3 className="margin-bottom">Maçonnerie</h3>
-                        <p>
-
-                            Outre la maçonnerie, le stagiaire pourra réaliser des travaux
-                            di'solation et d'étanchéité, des coffrages et ferraillages, du
-                            bétonnage, jointoiement et égouttage.
-                        </p>
-                        <ul>
-                            <li>Inscription toute l’année</li>
-                            <li>Formations qualifiante /+ou - 9 mois / Lundi - Jeudi</li>
-                            <li>Stage en entreprise / 4 semaines / Lundi - Vendredi</li>
-                            <li>[Objectif: mise à l’emploi]</li>
-                            <li>Contrat BF: 2€/heure, etc.</li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="pic1"></div>
-            </div> */}
         </section>
     );
 };
