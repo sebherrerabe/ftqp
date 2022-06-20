fbf75fad38561d99ea68fdc3b8beddc88983c3a5
Kaat Willems <97091300+KaatWillems@users.noreply.github.com>
2022-06-20 16:42:24 +0200

    fixed gallery, almost finished

--- src/Sections/Gallery/Gallery.jsx
+++ src/Sections/Gallery/Gallery.jsx
@@ -18,77 +18,77 @@ const Gallery = () => {
 
          
 
-    return (<section className="section main-container" id='galerie'>
-        <div className="gallery-container bg-white">
-            <Title title="Galerie" direction="left" icon={faImage}></Title>
-        </div>
+    return (
+<section className="section main-container" id='galerie'>
+    <div className='gallery-top'>
+        <Title title="Galerie" direction="left" icon={faImage}></Title>
+    </div>
+    <div className="gallery-container bg-white">
 
         <Carousel
-  additionalTransfrom={0}
-  arrows
-  autoPlay
-  autoPlaySpeed={4000}
-  centerMode={false}
-  className=""
-  containerClass="container-with-dots"
-  dotListClass=""
-  draggable
-  focusOnSelect={false}
-  infinite
-  itemClass=""
-  keyBoardControl
-  minimumTouchDrag={80}
-  pauseOnHover
-  renderArrowsWhenDisabled={false}
-  renderButtonGroupOutside={false}
-  renderDotsOutside={false}
-  responsive={{
-    desktop: {
-      breakpoint: {
-        max: 3000,
-        min: 1024
-      },
-      items: 3,
-      partialVisibilityGutter: 40
-    },
-    mobile: {
-      breakpoint: {
-        max: 464,
-        min: 0
-      },
-      items: 1,
-      partialVisibilityGutter: 30
-    },
-    tablet: {
-      breakpoint: {
-        max: 1024,
-        min: 464
-      },
-      items: 2,
-      partialVisibilityGutter: 30
-    }
-  }}
-  rewind={false}
-  rewindWithAnimation={false}
-  rtl={false}
-  shouldResetAutoplay
-  showDots={false}
-  sliderClass=""
-  slidesToSlide={2}
-  swipeable
->
+            additionalTransfrom={0}
+            arrows
+            autoPlay
+            autoPlaySpeed={4000}
+            centerMode={false}
+            className=""
+            containerClass="container-with-dots"
+            dotListClass=""
+            draggable
+            focusOnSelect={false}
+            infinite
+            itemClass=""
+            keyBoardControl
+            minimumTouchDrag={80}
+            pauseOnHover
+            renderArrowsWhenDisabled={false}
+            renderButtonGroupOutside={false}
+            renderDotsOutside={false}
+            responsive={{
+                desktop: {
+                breakpoint: {
+                    max: 3000,
+                    min: 1024
+                },
+                items: 3,
+                partialVisibilityGutter: 40
+                },
+                mobile: {
+                breakpoint: {
+                    max: 464,
+                    min: 0
+                },
+                items: 1,
+                partialVisibilityGutter: 30
+                },
+                tablet: {
+                breakpoint: {
+                    max: 1024,
+                    min: 464
+                },
+                items: 2,
+                partialVisibilityGutter: 30
+                }
+            }}
+            rewind={false}
+            rewindWithAnimation={false}
+            rtl={false}
+            shouldResetAutoplay
+            showDots={false}
+            sliderClass=""
+            slidesToSlide={2}
+            swipeable
+            >
 
-    <Image src={chantier9} alt={"text"} > </Image>
-    <Image src={chantier1} alt={"text"} > </Image>
-    <Image src={chantier4} alt={"text"} > </Image>
-    <Image src={chantier6} alt={"text"} > </Image>
-    <Image src={chantier2} alt={"text"} > </Image>
+            <Image src={chantier9} alt={"text"} > </Image>
+            <Image src={chantier1} alt={"text"} > </Image>
+            <Image src={chantier4} alt={"text"} > </Image>
+            <Image src={chantier6} alt={"text"} > </Image>
+            <Image src={chantier2} alt={"text"} > </Image>
 
-  
-  
-</Carousel>
-
-    </section>);
-}
+        </Carousel>
+    </div>
+</section>
+);}
 
 export default Gallery;
