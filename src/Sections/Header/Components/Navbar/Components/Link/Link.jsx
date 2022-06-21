b30d3a226a2a464aa604081ca8436390b3d233f4
sebherrerabe <sebastianherrerac1997@gmail.com>
2022-06-21 16:21:19 +0200

    navbar ready for tablettes

--- src/Sections/Header/Components/Navbar/Components/Link/Link.jsx
+++ src/Sections/Header/Components/Navbar/Components/Link/Link.jsx
@@ -1,12 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Link = ({ text, icon, active, id }) => {
+const Link = ({ text, icon, active, id, setNavbarTranslate }) => {
 
     const capitaliseAll = (str) => {
         return str.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
     }
     return (<li className={active ? 'active' : ''}>
-        <a href={id}><FontAwesomeIcon icon={icon} />{capitaliseAll(text)}</a>
+        <a href={id} onClick={() => setNavbarTranslate("-100%")}><FontAwesomeIcon icon={icon} />{capitaliseAll(text)}</a>
         <div className="bar"></div>
     </li>);
 }
