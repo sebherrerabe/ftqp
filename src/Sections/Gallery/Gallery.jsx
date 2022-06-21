18504af00fdeb4ea73b9442d92e37cd11065951f
Kaat Willems <97091300+KaatWillems@users.noreply.github.com>
2022-06-21 15:15:47 +0200

    email working. addede more pics in gallery

--- src/Sections/Gallery/Gallery.jsx
+++ src/Sections/Gallery/Gallery.jsx
@@ -9,12 +9,35 @@ import Image from './Components/Image'
 
 
 import { faImage } from '@fortawesome/free-solid-svg-icons';
-import chantier from './assets/chantier.jpeg'
-import chantier1 from './assets/chantier1.jpeg'
-import chantier2 from './assets/chantier2.jpeg'
-import chantier9 from './assets/chantier9.jpeg'
-import chantier4 from './assets/chantier4.jpeg'
-import chantier6 from './assets/chantier6.jpeg'
+    import chantier from './assets/chantier.jpeg'
+    import chantier1 from './assets/chantier1.jpeg'
+    import chantier2 from './assets/chantier2.jpeg'
+    import chantier4 from './assets/chantier4.jpeg'
+    import chantier6 from './assets/chantier6.jpeg'
+    import chantier9 from './assets/chantier9.jpeg'
+    import group from './assets/group.jpeg'
+    import group2 from './assets/group2.jpeg'
+    import group3 from './assets/group3.jpeg'
+    import maconnerie from './assets/maconnerie.jpeg'
+    import maconnerie2 from './assets/maconnerie2.jpeg'
+    import maconnerie3 from './assets/maconnerie3.jpeg'
+    import maconnerie4 from './assets/maconnerie4.jpeg'
+    import maconnerie5 from './assets/maconnerie5.jpeg'
+    import maconnerie6 from './assets/maconnerie6.jpeg'
+    import maconnerie7 from './assets/maconnerie7.jpeg'
+    import maconnerie11 from './assets/maconnerie11.jpeg'
+    import maconnerie13 from './assets/maconnerie13.jpeg' 
+    import plafonnage from './assets/plafonnage.jpeg'
+    import plafonnage2 from './assets/plafonnage2.jpeg'
+    import plafonnage3 from './assets/plafonnage3.jpeg'
+    import plafonnage4 from './assets/plafonnage4.jpeg'
+    import plafonnage5 from './assets/plafonnage5.jpeg'
+    import plafonnage6 from './assets/plafonnage6.jpeg'
+    import plafonnage7 from './assets/plafonnage7.jpeg'
+    import plafonnage9 from './assets/plafonnage9.jpeg'
+    import plafonnage12 from './assets/plafonnage12.jpeg'
+  
+    
 
 
 
@@ -84,11 +107,39 @@ const Gallery = () => {
                     swipeable
                 >
 
-                    <Image src={chantier9} alt={"text"} > </Image>
-                    <Image src={chantier1} alt={"text"} > </Image>
-                    <Image src={chantier4} alt={"text"} > </Image>
-                    <Image src={chantier6} alt={"text"} > </Image>
-                    <Image src={chantier2} alt={"text"} > </Image>
+                    
+                    <Image src={chantier9} alt={"chantier"} > </Image>
+                    <Image src={group} alt={"group"} > </Image>
+                    
+                    <Image src={maconnerie2} alt={"maconnerie"} > </Image>
+                    <Image src={maconnerie3} alt={"maconnerie"} > </Image>
+                    <Image src={maconnerie5} alt={"maconnerie"} > </Image>
+                    <Image src={maconnerie7} alt={"maconnerie"} > </Image>
+                    <Image src={maconnerie13} alt={"maconnerie"} > </Image>
+                    <Image src={maconnerie6} alt={"maconnerie"} > </Image>
+                    <Image src={maconnerie4} alt={"maconnerie"} > </Image>
+                    <Image src={plafonnage9} alt={"plafonnage"} > </Image>
+                    <Image src={plafonnage} alt={"plafonnage"} > </Image>
+                    <Image src={plafonnage2} alt={"plafonnage"} > </Image>
+                    <Image src={plafonnage3} alt={"plafonnage"} > </Image>
+                    <Image src={plafonnage4} alt={"plafonnage"} > </Image>
+                    <Image src={plafonnage5} alt={"plafonnage"} > </Image>
+                    <Image src={plafonnage6} alt={"plafonnage"} > </Image>
+                    <Image src={plafonnage7} alt={"plafonnage"} > </Image>
+                    
+                    <Image src={plafonnage12} alt={"plafonnage"} > </Image>
+                    <Image src={chantier6} alt={"plafonnage"} > </Image>
+                   
+                    
+                    <Image src={group2} alt={"group-pic"} > </Image>
+                    <Image src={maconnerie} alt={"maconnerie"} > </Image>
+                    <Image src={maconnerie11} alt={"maconnerie"} > </Image>
+                    <Image src={group3} alt={"team"} > </Image>
+
+                    <Image src={chantier4} alt={"chantier"} > </Image>
+                    <Image src={chantier2} alt={"chantier"} > </Image>  
+                    <Image src={chantier1} alt={"chantier"} > </Image>
+                    <Image src={chantier} alt={"chantier"} > </Image>
 
                 </Carousel>
             </div>
