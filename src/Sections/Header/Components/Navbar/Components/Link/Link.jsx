a965b350c454a26b5fc7ae303fc82b47d8a3a865
sebherrerabe <sebastianherrerac1997@gmail.com>
2022-06-21 16:48:32 +0200

    navbar working perfectly for tablettes

--- src/Sections/Header/Components/Navbar/Components/Link/Link.jsx
+++ src/Sections/Header/Components/Navbar/Components/Link/Link.jsx
@@ -6,7 +6,7 @@ const Link = ({ text, icon, active, id, setNavbarTranslate }) => {
         return str.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
     }
     return (<li className={active ? 'active' : ''}>
-        <a href={id} onClick={() => setNavbarTranslate("-100%")}><FontAwesomeIcon icon={icon} />{capitaliseAll(text)}</a>
+        <a href={id} onClick={() => setNavbarTranslate(false)}><FontAwesomeIcon icon={icon} />{capitaliseAll(text)}</a>
         <div className="bar"></div>
     </li>);
 }
